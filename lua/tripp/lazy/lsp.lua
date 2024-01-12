2d1b6fee6378ce4b5bd85b3c76d5728224ea570a
Tripp Roberts <tripp@tripp-roberts.com>
2024-01-12 12:57:14 -0600

    add global vim to lua_ls

--- lua/tripp/lazy/lsp.lua
+++ lua/tripp/lazy/lsp.lua
@@ -1,67 +1,79 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/nvim-cmp',
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-    },
-    config = function()
-        require("mason").setup();
-        require("mason-lspconfig").setup({
-            ensure_installed = { 'tsserver', 'lua_ls' },
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup {}
-                end
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+  },
+  config = function()
+    require("mason").setup();
+    require("mason-lspconfig").setup({
+      ensure_installed = { 'tsserver', 'lua_ls' },
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup {}
+        end,
+        ["lua_ls"] = function()
+          local lspconfig = require('lspconfig')
+          lspconfig.lua_ls.setup {
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { 'vim' },
+                },
+              },
             },
-        });
-        local cmp = require 'cmp'
-        cmp.setup({
-            enabled = function()
-                local context = require("cmp.config.context")
-                local disabled = false
-                disabled = disabled or (vim.api.nvim_buf_get_option(0, "buftype") == "prompt")
-                disabled = disabled or (vim.fn.reg_recording() ~= "")
-                disabled = disabled or (vim.fn.reg_executing() ~= "")
-                disabled = disabled or context.in_treesitter_capture("comment")
-                return not disabled
-            end,
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping(function(fallback)
-                    -- This little snippet will confirm with enter, and if no entry is selected, will confirm the first item
-                    if cmp.visible() then
-                        local entry = cmp.get_selected_entry()
-                        if not entry then
-                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                        else
-                            cmp.confirm()
-                        end
-                    else
-                        fallback()
-                    end
-                end, { "i", "s", "c", }),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }
-            }, {
-                { name = 'buffer' },
-            })
-        })
-    end,
+          }
+        end
+      },
+    });
+    local cmp = require 'cmp'
+    cmp.setup({
+      enabled = function()
+        local context = require("cmp.config.context")
+        local disabled = false
+        disabled = disabled or (vim.api.nvim_buf_get_option(0, "buftype") == "prompt")
+        disabled = disabled or (vim.fn.reg_recording() ~= "")
+        disabled = disabled or (vim.fn.reg_executing() ~= "")
+        disabled = disabled or context.in_treesitter_capture("comment")
+        return not disabled
+      end,
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping(function(fallback)
+          -- This little snippet will confirm with enter, and if no entry is selected, will confirm the first item
+          if cmp.visible() then
+            local entry = cmp.get_selected_entry()
+            if not entry then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              cmp.confirm()
+            end
+          else
+            fallback()
+          end
+        end, { "i", "s", "c", }),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }
+      }, {
+        { name = 'buffer' },
+      })
+    })
+  end,
 }
