7998a9e00e547631620599f75b0ef636bda4abe5
Tripp Roberts <tripp@tripp-roberts.com>
2024-01-16 20:15:20 -0500

    fix trouble icons config

--- lua/tripp/lazy/trouble.lua
+++ lua/tripp/lazy/trouble.lua
@@ -1,10 +1,10 @@
 return {
-    "folke/trouble.nvim",
-    opts = {
-        icons = false,
-    },
-    config = function()
-        local trouble = require("trouble")
-        vim.keymap.set("n", "<leader>tt", function() trouble.toggle() end)
-    end
+  "folke/trouble.nvim",
+  config = function()
+    local trouble = require("trouble")
+    trouble.setup({
+      icons = false
+    })
+    vim.keymap.set("n", "<leader>tt", function() trouble.toggle() end)
+  end
 }
