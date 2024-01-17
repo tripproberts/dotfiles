97c7f54407bd3a4688114e476217dd2f8da815f3
Tripp Roberts <tripp@tripp-roberts.com>
2024-01-17 00:13:06 -0500

    use telescope for lsp find references

--- lua/tripp/init.lua
+++ lua/tripp/init.lua
@@ -4,11 +4,12 @@ require("tripp.remap")
 require("tripp.autocommands")
 
 vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('TrippGroup', {}),
-    callback = function(ev)
-        local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("n", "<leader>fr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>b", function() vim.lsp.buf.definition() end, opts)
-    end
+  group = vim.api.nvim_create_augroup('TrippGroup', {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('n', '<leader>fr',
+      function() require('telescope.builtin').lsp_references({ noremap = true, silent = true }) end, opts)
+    vim.keymap.set("n", "<leader>b", function() vim.lsp.buf.definition() end, opts)
+  end
 })
