f410259e73626aa78e927afadc444c409815fb2a
Tripp Roberts <tripp@tripp-roberts.com>
2024-01-17 22:59:32 -0500

    start grep_search in normal mode

--- lua/tripp/lazy/fuzzy.lua
+++ lua/tripp/lazy/fuzzy.lua
@@ -1,15 +1,22 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {
-        "plenary"
-    },
-    config = function() 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>o', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>h', builtin.oldfiles, {})
-        vim.keymap.set('n', '<leader>f', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ")});
-        end)
-    end
+  'nvim-telescope/telescope.nvim',
+  dependencies = {
+    "plenary"
+  },
+  config = function()
+    require('telescope').setup({
+      pickers = {
+        grep_string = {
+          initial_mode = "normal",
+        }
+      }
+    })
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>o', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>h', builtin.oldfiles, {})
+    vim.keymap.set('n', '<leader>f', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    end)
+  end
 }
