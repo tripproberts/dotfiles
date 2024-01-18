f278dfc4492f86d09b0ec0e913d1ee65a8e5a404
Tripp Roberts <tripp@tripp-roberts.com>
2024-01-17 20:53:17 -0500

    add json formatting

--- lua/tripp/lazy/format.lua
+++ lua/tripp/lazy/format.lua
@@ -1,27 +1,27 @@
 return {
-        "stevearc/conform.nvim",
-        config = function()
-            require("conform").setup({
-                formatters_by_ft = {
-                    lua = { "stylua" },
-                    python = { "isort", "ruff" },
-                    javascript = { { "prettierd", "prettier" } },
-                    javascriptreact = { { "prettierd", "prettier" } },
-                    typescript = { { "prettierd", "prettier" } },
-                    typescriptreact = { { "prettierd", "prettier" } },
-                    markdown = { { "prettierd", "prettier" } },
-                    mdx = { { "prettierd", "prettier" } },
-                    json = { { "prettierd", "prettier" } },
-                    css = { { "prettierd", "prettier" } },
-                    html = { { "prettierd", "prettier" } },
-                    yaml = { { "prettierd", "prettier" } },
-                },
-                format_on_save = {
-                    -- These options will be passed to conform.format()
-                    timeout_ms = 500,
-                    lsp_fallback = true,
-                },
-                notify_on_error = false,
-            })
-        end,
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "ruff" },
+        javascript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        markdown = { { "prettierd", "prettier" } },
+        mdx = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier", "fixjson" } },
+        css = { { "prettierd", "prettier" } },
+        html = { { "prettierd", "prettier" } },
+        yaml = { { "prettierd", "prettier" } },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      notify_on_error = false,
+    })
+  end,
 }
