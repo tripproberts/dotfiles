f633dfae2fa6e204f26f5419a1673ffc9f70ceb9
Tripp Roberts <tripp@tripp-roberts.com>
2024-01-20 23:24:31 -0500

    remove unused python formatters

--- lua/tripp/lazy/format.lua
+++ lua/tripp/lazy/format.lua
@@ -4,7 +4,6 @@ return {
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { "isort", "ruff" },
         javascript = { { "prettierd", "prettier" } },
         javascriptreact = { { "prettierd", "prettier" } },
         typescript = { { "prettierd", "prettier" } },
