30694367e72a2b1d936d4d3f051bdb7ed9217c26
Tripp Roberts <tripp@tripp-roberts.com>
2024-01-22 20:13:07 -0500

    add keymaps for git gutter stage and undo hunk

--- lua/tripp/lazy/git.lua
+++ lua/tripp/lazy/git.lua
@@ -5,6 +5,8 @@ return {
 			vim.keymap.set("n", "<leader>hn", vim.cmd.GitGutterNextHunk)
 			vim.keymap.set("n", "<leader>hp", vim.cmd.GitGutterPrevHunk)
 			vim.keymap.set("n", "<leader>hd", vim.cmd.GitGutterPreviewHunk)
+			vim.keymap.set("n", "<leader>hs", vim.cmd.GitGutterStageHunk)
+			vim.keymap.set("n", "<leader>hu", vim.cmd.GitGutterUndoHunk)
 		end,
 	},
 	{
