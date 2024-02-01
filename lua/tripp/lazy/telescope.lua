97f86a64ff7805c97c0f815a58cc3127087e9461
Tripp Roberts <tripp@tripp-roberts.com>
2024-02-01 13:45:42 -0500

    set keymap for telescope help tags

--- lua/tripp/lazy/telescope.lua
+++ lua/tripp/lazy/telescope.lua
@@ -184,7 +184,7 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>o", builtin.find_files, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>h", builtin.oldfiles, {})
+		vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
 		vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
 		vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
 		vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
